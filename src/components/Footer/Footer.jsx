4d02f71cb8983bae38805219b108d68b9f1e74b6
my first github repo <mrarifmondol98@gmail.com>
2026-01-04 04:03:10 -0800

    added footer description

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                 <div className='grid grid-cols-1 sm:grid-cols-5 gap-6 mt-4 text-white py-10 px-2 xl:px-0'>
                     <div>
                         <h4 className='font-bold text-2xl mb-3'>Cs - Ticket System</h4>
-                        <p className='text-[16px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p className='text-[16px]'>CS — Ticket System is a centralized customer support platform designed to track, prioritize, and resolve user issues efficiently. It manages customer tickets across authentication, payments, billing, app stability, subscriptions, and communications, clearly categorizing them by status (Open, In-Progress) and priority (High, Medium, Low).</p>
                     </div>
                     <div>
                         <h4 className='font-bold text-2xl mb-3'>Company</h4>
